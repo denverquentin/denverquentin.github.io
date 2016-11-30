baf720e9632e49254e46b3c9b12e5ab33db85602
Quentin Fish <quentin.fish@roundcorner.com>
2016-11-30 16:40:09 -0700

    static resources test for rc

---
+++ rc/js/utils.js
@@ -0,0 +1,123 @@
+rc = rc || {};
+rc.utils = rc.utils || {};
+
+
+console.log('var rc.ns in utils.js = ' + rc.ns);
+
+
+/* Helper to convert quoted HTML into text */
+rc.text = function(text) {
+	return $('<div></div>').html(text).text();
+};
+
+rc.html = function(text) {
+	return $('<div></div>').html(text).html();
+};
+
+/* Encode / decode html entities */
+rc.html_encode = function(text) {
+	text = text || "";
+	text = text.replace(new RexExp('&', 'g'), '&' + 'amp;');
+	text = text.replace(new RexExp('"', 'g'), '&' + 'quot;');
+	text = text.replace(new RexExp('\'', 'g'), '&' + '#39;');
+	text = text.replace(new RexExp('<', 'g'), '&' + 'lt;');
+	text = text.replace(new RexExp('>', 'g'), '&' + 'gt;');
+	return text;
+};
+
+rc.html_decode = function(text) {
+	text = text || "";
+	text = text.replace(new RegExp('&' + 'amp;', 'g'), '&');
+	text = text.replace(new RegExp('&' + 'quot;', 'g'), '"');
+	text = text.replace(new RegExp('&' + '#39;', 'g'), '\'');
+	text = text.replace(new RegExp('&' + 'lt;', 'g'), '<');
+	text = text.replace(new RegExp('&' + 'gt;', 'g'), '>');
+	return text;
+};
+
+/* Helper to convert string date to SF number date */
+rc.date = function(originalDate) {
+	if (originalDate) {
+		return new Date(originalDate).getTime();
+	} else {
+		return null;
+	}
+};
+
+rc.cleanKeysToLower = function(sourceObject){
+	var key;
+	var keys = rc.getKeys(sourceObject);
+	var n = keys.length;
+	var targetObject={}
+	while (n--) {
+		key = keys[n];
+		targetObject[key.toLowerCase()] = sourceObject[key];
+	}
+	return targetObject;
+};
+
+/* Create a random ID for the component */
+rc.guid = function() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+		return v.toString(16);
+	});
+};
+
+rc.stripTags = function(valueText,tag) {
+	var s = valueText || '';
+	s = s.replace(RegExp('<\/?' + tag + '[^<>]*>', 'gi'), '');
+	return s;
+};
+
+rc.getKeys = function (obj) {
+	var r = []
+	for (var k in obj) {
+		if (!obj.hasOwnProperty(k))
+			continue
+		r.push(k)
+	}
+	return r;
+};
+
+/* filter / decode data for components */
+rc.utils.filterComponentData = function(componentsArray) {
+	rc.context(componentsArray).each(function(index, component_data) {
+		var data = component_data || {};
+		data.data = data.data || {};
+		data.type = data.type || '';
+		data.styles = data.styles || {};
+		data.defaultValues = data.defaultValues || {};
+	});
+	return componentsArray;
+}
+
+rc.setModeView = function() {
+	var returnVal = rc.setParam('mode', 'view');
+	rc.events.trigger('view-change',false);
+	return returnVal;
+};
+
+rc.setModeEdit = function() {
+	var returnVal = rc.setParam('mode', 'edit');
+	rc.events.trigger('view-change',true);
+	return returnVal;
+};
+
+rc.setModeFlow = function() {
+	var returnVal = rc.setParam('mode', 'flow');
+	rc.events.trigger('view-change',false);
+	return returnVal;
+};
+
+/* utils/library extensions */
+Number.prototype.formatMoney = function(c, d, t) {
+	var n = this, 
+	c = isNaN(c = Math.abs(c)) ? 2 : c, 
+	d = d == undefined ? "." : d, 
+	t = t == undefined ? "," : t, 
+	s = n < 0 ? "-" : "", 
+	i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+	j = (j = i.length) > 3 ? j % 3 : 0;
+	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
