fb78921f2c2e2ce9ff6764c4c4c5244548c251be
Quentin Fish <quentin.fish@roundcorner.com>
2016-12-12 22:20:40 -0700

    asdf

--- rc/js/rc.js
+++ rc/js/rc.js
@@ -1478,6 +1478,35 @@ rc.components.Button.execute = function() {
 	}
 }
 
+rc.components.registerMergeFieldAutoComplete = function(field,dataArray) {
+	function split(val) {return val.split( /,\s*/ );}
+	function extractLast(term) {return split( term ).pop();}
+	// don't navigate away from the field on tab when selecting an item
+	$(field).bind( "keydown", function(event) {
+		if (event.keyCode === $.ui.keyCode.TAB && $(this).data("ui-autocomplete").menu.active) {
+			event.preventDefault();
+		}
+	}).autocomplete({
+		minLength: 0,
+		source: function(request,response) {
+		// delegate back to autocomplete, but extract the last term
+		response($.ui.autocomplete.filter(dataArray,extractLast(request.term)));
+		},
+		focus:function() {return false;},
+		select: function(event,ui) {
+			var terms = split(this.value);
+			// remove the current input
+			terms.pop();
+			// add the selected item
+			terms.push(ui.item.value);
+			// add placeholder to get the comma-and-space at the end
+			terms.push("");
+			this.value = terms.join(", ");
+			return false;
+		}
+	});
+};
+
 rc.components.insertLitleProfilingTag = function() {
 	//create profiling element, find form body, add profiling element to form body
 	var fraudDetectTokenGenHtml = rc.context("#rc-litle-advance-fraud-detection").html();
