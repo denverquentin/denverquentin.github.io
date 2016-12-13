15d355238afced205246e9b1d3407bd7b461dcbd
Quentin Fish <quentin.fish@roundcorner.com>
2016-12-13 10:21:56 -0700

    asdf

--- rc/js/rc.form.edit.js
+++ rc/js/rc.form.edit.js
@@ -2,11 +2,9 @@
 Feel free to put loads of comments in the code and be sure to minify
 this script any time it's edited. The Campaign_DesignForm.page should
 always load the minified version. */
-console.log('LOADED rc.form.edit.js - MUST BE IN EDIT MODE!!!!');
 rc = rc || {};
 rc.ui = rc.ui || {};
 rc.modal = rc.modal || {};
-rc.components = rc.components || {};
 
 rc.initializeFormAppInDesignMode = function() {
 	console.log('rc.initializeFormAppInDesignMode');
@@ -17,7 +15,7 @@ rc.initializeFormAppInDesignMode = function() {
 	//on view change refresh html block elements to toggle between html<->text views
 	rc.events.on('view-change',rc.components.HtmlBlock.refreshView);
 	// on view change, toggle placeholder values shown in fields
-	rc.events.on('view-change',rc.rollupPlaceholderValues);// todo: changed view-cahnge to view-change
+	rc.events.on('view-change',rc.rollupPlaceholderValues);
 	// on view change, toggle default values shown in fields
 	rc.events.on('view-change',rc.rollupDefaultValues);
 	rc.initializeModals();
@@ -107,7 +105,7 @@ rc.initializeHeaderButtons = function() {
 	});
 };
 
-rc.validateGivingPaymentFields = function() {/* this validation is only for the form builder */
+rc.validateGivingPaymentFields = function() {
 	//is giving frequency field on the page
 	var hasGivingFrequencyField = $('.rc-component-merge-field-content').find('[name="'+rc.ns+'giving_giving_frequency__c"]').val() != undefined ? true : false;
 	//is giving amount field on the page
@@ -235,9 +233,7 @@ rc.ui.releaseProcessingModal = function() {
 	rc.ui.showProcessingModal.queue.pop();
 	if (rc.ui.showProcessingModal.queue.length == 0) {rc.context('#rc-modal-processing').modal('hide');}
 }
-
 rc.ui.showProcessingModal.queue = [];
-
 
 
 rc.modal.loadContainerCSS = function() {
@@ -500,6 +496,342 @@ rc.modal.confirmInsertForm = function() {
 	rc.upsertFormData({name:modal.find('.modal-body input[type="text"]').val(),type:'insert'});
 	rc.selectFormInfoList();// Reselect form name list
 	rc.context(this).closest('.modal').modal('hide');// Dismiss modal
+};
+
+
+rc.upsertFormData = function(send, deferred) {
+	rc.console.debug('rc.upsertFormData send..',send);
+	//validate campaign ask merge fields: Giving payment frequency, Giving amount
+	rc.validateGivingPaymentFields();
+	// Options
+	send = send || {};
+	send.name = send.name || rc.context('#rc-form-name-list').siblings().find('.dropdown-toggle-text').text();
+	send.type = send.type || 'update';
+	send.form = { name: send.name, containers: [], workflows: [], json_version: "0.2", data: {} , styles:{} };
+	deferred = deferred || new jQuery.Deferred();
+	// Theme
+	send.form.data['theme-name'] = rc.context('#rc-theme-link').attr('data-name');
+	send.form.data['theme-href'] = rc.context('#rc-theme-link').attr('href');
+	//save validator on/off flag
+	send.form.data['validations-enabled'] = rc.context("#validations-enabled").is(":checked") ? "true" : "false";
+	// Save Page level Styles
+	rc.context(rc.context("html").get(0).attributes).each(function(index, attr) {
+		if (attr.name.match('css-')) {
+			var name = attr.name.replace('css-', '');
+			send.form.styles[name] = attr.value;
+		}
+	});
+	rc.context('#rc-container-list').find('.rc-container-column-list').each(function() {
+		rc.upsertFormData.exportFormContainer(send.form.containers, this);
+	});
+	rc.context('#rc-workflows-list').find('.rc-container-workflow').each(function() {
+		rc.upsertFormData.exportFormWorkflow(send.form.workflows, this);
+	});
+	// Toggle the button if error fail the save
+	if (rc.context('.input-group.has-error,.form-group.has-error').length != 0) {
+		// Toggle the button
+		rc.context('.page-header [data-action="rc-action-save"]').button('error').removeClass("btn-success").addClass("btn-danger");
+		return;
+	} else {
+		rc.context('.page-header [data-action="rc-action-save"]').button('saving').removeClass("btn-danger").addClass("btn-success");
+	}
+	// Send to salesforce
+	if ('insert' == send.type) {
+		rc.remoting.invokeAction(rc.actions.upsertFormData, rc.campaignId, null, send.form, function(recv, meta) {
+			rc.upsertFormData.done(deferred, send, recv, meta);
+		});
+		rc.ui.markProcessing();
+	}
+	if ('update' == send.type) {
+		rc.remoting.invokeAction(rc.actions.upsertFormData, rc.campaignId, rc.getParam('form'), send.form, function(recv, meta) {
+			rc.upsertFormData.done(deferred, send, recv, meta);
+		});
+		rc.ui.markProcessing();
+	}
+	return deferred.promise();
+};
+
+rc.upsertFormData.done = function(deferred, send, recv, meta) {
+	rc.console.debug('rc.upsertFormData.done');
+	rc.console.debug('.. deferred', deferred);
+	rc.console.debug('.. send', send);
+	rc.console.debug('.. recv', recv);
+	rc.console.debug('.. meta', meta);
+	recv = recv || {};
+	rc.context('.page-header [data-action="rc-action-save"]').button('save');// Toggle the button
+	rc.ui.markProcessingDone({modified:false});// Unmark processing
+	// If the returned ID is different from the current one, redirect
+	if (rc.getParam('form') != recv.id) {
+		rc.setParam('form', recv.id);
+		rc.selectFormInfoList();
+	} else {
+		rc.selectFormData();
+	}
+	if (deferred && deferred.resolve) {deferred.resolve();}// Mark resolved?
+};
+
+rc.upsertFormData.exportFormContainer = function(list, item) {
+	var list = list || [];
+	var item = rc.context(item);
+	// Setup data
+	var data = {};
+	data.columns = [];
+	data.data = {};
+	data.data['columns'] = parseInt(item.attr('data-columns'));
+	data.data['guid'] = item.attr('id') || rc.guid();
+	data.styles = {};
+	// Styles
+	rc.context(item.get(0).attributes).each(function(index, attr) {
+		if (attr.name.match('css-')) {
+			var name = attr.name.replace('css-', '');
+			data.styles[name] = attr.value;
+		}
+	});
+	item.find('.rc-container-column').each(function() {
+		rc.upsertFormData.exportFormColumn(data.columns, this);
+	});
+	list.push(data);
+};
+
+rc.upsertFormData.exportFormColumn = function(list, item) {
+	var list = list || [];
+	var item = rc.context(item);
+	// Setup data
+	var data = {};
+	data.components = [];
+	data.data = {};
+	data.data['position'] = parseInt(item.attr('data-position'));
+	data.data['size'] = parseInt(item.attr('data-size'));
+	item.find('.rc-component').each(function() {
+		rc.upsertFormData.exportFormComponent(data.components, this);
+	});
+	list.push(data);
+};
+
+rc.upsertFormData.exportFormComponent = function(list, item) {
+	var list = list || [];
+	var item = rc.context(item);
+	// Setup data
+	var data = {};
+	data.styles = {};
+	data.type = item.attr('data-component-type');
+	data.data = {};
+	data.data['guid'] = item.attr('id') || rc.guid();
+	data.data['customHidden'] = item.find(".rc-component-content").attr("data-field-hidden");
+	data.defaultValues = {};
+	data.placeholderValues = {};
+	if (data.type == 'advanced-css') {
+		data.data['text'] = item.find('textarea').val();
+	}
+	if (data.type == 'address') {
+		//create map of field name and required flags.
+		rc.context(rc.context(item).find("[data-field-name]")).each(function() {
+			var key = "req-"+rc.context(this).attr("data-field-name");
+			var value = rc.context(this).find("[data-required]").attr("data-required");
+			data.data[key] = value;
+			// map of defualt values (create map of field name and default values)
+			// map of placeholder values (create map of field name and placeholder values)
+			rc.context( rc.context(item).find('.rc-field-name') ).each(function() {
+				rc.upsertFormData.exportformDefaultValues(data.defaultValues, rc.context(this));
+				rc.upsertFormData.exportFormPlaceholderValues(data.placeholderValues, rc.context(this));
+			});
+		});
+	}
+	if (data.type == 'button') {
+		data.data['text'] = item.find('.rc-component-content .rc-name').text().trim();
+		data.data['workflow'] = item.attr('data-workflow');
+	}
+	if (data.type == 'credit-card') {
+		//save data for all hidden fields
+		var component = rc.context(item);
+		component.find('[data-field-hidden="true"]').each(function(index,hiddenField) {
+			var formControlInput = rc.context(hiddenField).find(".form-control");
+			var fieldName = formControlInput.attr("name");
+			if (!fieldName || fieldName=='') {
+				return true;
+			}
+			var fieldValue = '';
+			if (formControlInput.attr("type")=="text") {
+				fieldValue = formControlInput.val();
+			} else if (formControlInput.attr("type")=="checkbox") {
+				fieldValue = formControlInput.is(":checked") ? "true" : "false";
+			}
+			data.data[fieldName] = fieldValue;
+		});
+		// map of placeholder values
+		rc.context(rc.context(item).find("[data-field-name]") ).each(function() {
+			rc.context( rc.context(item).find('.rc-field-name') ).each(function() {
+				rc.upsertFormData.exportformDefaultValues(data.defaultValues, rc.context(this));
+				rc.upsertFormData.exportFormPlaceholderValues(data.placeholderValues, rc.context(this));
+			});
+		});
+	}
+	if (data.type == 'external-stylesheet') {
+		data.data['text'] = item.find('.rc-component-content .form-control').val();
+	}
+	if (data.type == 'external-javascript') {
+		data.data['text'] = item.find('.rc-component-content .form-control').val();
+	}
+	if (data.type == 'internal-javascript') {
+		data.data['text'] = item.find('.rc-component-content .form-control').val();
+	}
+	if (data.type == 'image') {
+		data.data['data'] = item.find('img').attr('src');
+	}
+	if (data.type == 'jumbotron') {
+		data.data['header'] = item.find('.rc-header').text();
+		data.data['text'] = item.find('.rc-text').text();
+	}
+	if (data.type == 'merge-field') {
+		rc.console.debug('.. rc.upsertFormData.exportFormComponent - default : ', item.attr("data-field-default"));
+		data.data['default'] = item.find('.rc-field-name').attr('data-field-default');
+		data.data['hidden'] = item.attr('data-hidden');
+		data.data['name'] = item.find('.rc-field-name').attr('name');
+		data.data['required'] = item.find('.input-group').attr('data-required') == 'true';
+		data.data['text'] = item.find('.rc-field-text').text();
+		data.data['type'] = item.attr('data-type');
+		// map of defualt values
+		rc.upsertFormData.exportformDefaultValues(data.defaultValues, rc.context(item.find('.rc-field-name')));
+		rc.upsertFormData.exportFormPlaceholderValues(data.placeholderValues, rc.context(item.find('.rc-field-name')));
+	}
+	if (data.type == 'simple-header') {
+		data.data['text'] = item.find('.rc-component-content').text();
+	}
+	if (data.type == 'simple-text') {
+		data.data['text'] = item.find('.rc-component-content').text();
+	}
+	if (data.type == 'html-block') {
+		data.data['text'] = rc.context.data(item.find(".rc-component-html-content .rc-value")[0],"html-content");
+		if (data.data['text'].indexOf("<script") != -1) {
+			rc.ui.addMessageToComponent(item,'Script tag is not allowed in HTML block, will be removed.', rc.ui.WARNING);
+			rc.context(item).find('.form-group').addClass('has-error');
+		} else {
+			rc.context(item).find('.form-group').removeClass('has-error');
+		}
+		data.data['text'] = rc.stripTags(data.data["text"],"script");
+	}
+	if (data.type == 'url-link') {
+		data.data['label'] = item.find(".rc-url-label input.form-control").val();
+		data.data['link'] = item.find(".rc-url-link input.form-control").val();
+	}
+	if (data.type == 'campaign-ask') {
+		data.data['required'] = item.find('.input-group').attr('data-required') == 'true';
+		data.data['text-1'] = item.find(".text-1").text();
+		data.data['text-2'] = item.find(".text-2").text();
+		if (data.data['required'] == "false") {
+			item.find('.input-group').hide();
+		}
+	}
+	//rcEvents components
+	//export cart data
+	if (data.type == 'cart') {
+		data.data['header'] = item.find(".cart-header-text").text();
+		data.data['required'] = item.find('.input-group').attr('data-required') == 'true';
+		data.data = rc.components.Cart.populateSetupSaveData(item,data.data);
+	}
+	if (data.type == 'session') {
+		data.data['header'] = item.find('.session-header-text').text();
+		data.data['required'] = item.find('.input-group').attr('data-required') == 'true';
+		data.data = rc.components.Session.populateSetupSaveData(item,data.data);
+	}
+	if (data.type == 'attribute') {
+		data.data['attribute_id'] = item.find('.attribute-select').val();
+		data.data['productSlot'] = item.find('.rc-field-name').attr("name");
+		data.data['required'] = item.find('.input-group').attr('data-required') == 'true';
+	}
+	//save validators data
+	var validatorData = rc.validateInput.populateUpsertFormData(item);
+	data = $.extend(data, validatorData);
+	// Styles
+	rc.context(item.get(0).attributes).each(function(index, attr) {
+		if (attr.name.match('css-')) {
+			var name = attr.name.replace('css-', '');
+			data.styles[name] = attr.value;
+		}
+	});
+	list.push(data);
+};
+
+rc.upsertFormData.exportFormWorkflow = function(list, item) {
+	var list = list || [];
+	var item = rc.context(item);
+	// Setup data
+	var data = {};
+	data.actions = [];
+	data.data = {};
+	data.data['guid'] = item.attr('id') || rc.guid();
+	data.data['name'] = item.find('.rc-workflow-name').val();
+	data.data['active'] = item.find('.rc-workflow-active').is(':checked');
+	// Add actions
+	item.find('.rc-component').each(function() {
+		rc.upsertFormData.exportFormWorkflowAction(data.actions, this);
+	});
+	list.push(data);
+};
+
+rc.upsertFormData.exportFormWorkflowAction = function(list, item) {
+	var list = list || [];
+	var item = rc.context(item);
+	// Setup data
+	var data = {};
+	data.context = item.attr('data-context') || 'then';
+	data.method = item.attr('data-method') || '';
+	data.data = {};
+	data.data['guid'] = item.attr('id') || rc.guid();
+	if (data.method == 'send-mail') {
+		data.data['mail-to'] = item.attr('data-mail-to');
+		data.data['mail-reply-to'] = item.attr('data-mail-reply-to');
+		data.data['mail-subject'] = item.attr('data-mail-subject');
+		data.data['mail-body'] = item.attr('data-mail-body');
+	} else if (data.method == 'send-payment' && item.attr('data-value') == 'corduro') {
+		data.data['data'] = item.attr('data-value');
+		data.data['auth-token'] = item.attr('data-auth-token');
+		data.data['auth-only'] = item.attr('data-auth-only');
+		data.data['test-only'] = item.attr('data-test-only');
+	} else if (data.method == 'send-payment' && item.attr('data-value') == 'iATS') {
+		data.data['data'] = item.attr('data-value');
+	} else if (data.method == 'send-payment' && item.attr('data-value') == 'Litle') {
+		data.data['data'] = item.attr('data-value');
+		data.data['advanced-fraud-detection'] = item.attr('data-advanced-fraud-detection');
+		var isAdvancedFraudDetectionTestMode = item.attr('data-advanced-fraud-detection-test-mode');
+		data.data['advanced-fraud-detection-test-mode'] = isAdvancedFraudDetectionTestMode;
+		if (isAdvancedFraudDetectionTestMode == 'true') {
+			data.data['sessionId'] = item.attr('data-sessionId');
+		} else {
+			data.data['sessionId'] = '';
+		}
+	} else if (data.method == 'send-payment' && item.attr('data-value') == 'Authorize.net') {
+		data.data['data'] = item.attr('data-value');
+	} else if (data.method == 'send-payment' && item.attr('data-value') == 'Cybersource') {
+		data.data['data'] = item.attr('data-value');
+	} else if (data.method == 'copy-param') {
+		data.data['data'] = item.attr('data-value');
+		data.data['parameter'] = item.attr('data-parameter');
+	} else if (data.method ==  'send-data') {
+		data.data['exclude-giving'] = item.attr('exclude-giving');
+		data.data['exclude-events'] = item.attr('exclude-events');
+		data.data['data'] = item.attr('data-value');
+	} else {
+		data.data['data'] = item.attr('data-value');
+	}
+	list.push(data);
+};
+
+rc.upsertFormData.exportformDefaultValues = function(defaultValues, item) {
+	defaultValues = defaultValues || {};
+	item = rc.context(item);
+	var name = item.attr('name') || '';
+	defaultValues[name] = item.attr('data-field-default') || '';
+	return true;
+};
+
+rc.upsertFormData.exportFormPlaceholderValues = function(placeholderValues, item) {
+	rc.console.debug('rc.upsertFormData.exportFormPlaceholderValues');
+	placeholderValues = placeholderValues || {};
+	item = rc.context(item) || '';
+	var name = item.attr('name') || '';
+	placeholderValues[name] = item.attr('placeholder') || '';
+	return true;
 };
 
 rc.initializeFormAppInDesignMode();
