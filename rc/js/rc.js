6bbd605587ab4476b50f1d36bb14637662c1b623
Quentin Fish <quentinf@gmail.com>
2016-12-30 15:00:05 -0800

    sfg

--- rc/js/rc.js
+++ rc/js/rc.js
@@ -129,9 +129,9 @@ rc.selectData = function(deferred, send) {
 
 // todo: think this method always needs to execute - may rename if no longer need selectData
 rc.selectData.done = function(deferred, send, recv, meta) {
-	console.log('rc.selectData.done: send = ' + send);
-	console.log('rc.selectData.done: recv = ' + recv);
-	console.log('rc.selectData.done: meta = ' + meta);
+	console.log('rc.selectData.done: send = ' + JSON.stringify(send));
+	console.log('rc.selectData.done: recv = ' + JSON.stringify(recv));
+	console.log('rc.selectData.done: meta = ' + JSON.stringify(meta));
 	// Assign default values to all the fields
 	// This will be overwritten by field data values, if any.
 	rc.rollupDefaultValues();
