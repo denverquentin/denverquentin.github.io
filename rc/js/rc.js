b24fca95d627685a41c303f0fa56759933fb423b
Quentin Fish <quentin.fish@roundcorner.com>
2017-01-20 09:30:39 -0700

    asdf

--- rc/js/rc.js
+++ rc/js/rc.js
@@ -49,6 +49,7 @@ rc.initializeFormApp = function() {
 				// Add to workflow menu list
 				console.log('data.data.guid = ' + data.data.guid);
 				console.log('data.data.name = ' + data.data.name);
+				console.log('data.data.data = ' + data.data.data);
 				item_list.append(item.wrap('<li></li>').parent());
 			} catch (message) {
 				console.error('[ERROR]', message);
