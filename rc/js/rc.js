4b022e61c0422168f3b2b4dae9c1f5b89172339c
Quentin Fish <quentin.fish@roundcorner.com>
2017-01-23 21:14:49 -0700

    asdf

--- rc/js/rc.js
+++ rc/js/rc.js
@@ -3145,8 +3145,8 @@ rc.wf.process.LoadPage = function(deferred, action, data) {
 	var campaignFormId = rc.paramFormCampaignId;
 	if (campaignFormId == '') {campaignFormId=rc.campaignId;}
 	var redirectTo = rc.pageCampaignDesignForm + '?id=' + rc.campaignId
-		+ '&formCampaignId=' + campaignFormId + '&form=' + $(action).attr('data-value')
-		+ '&data=' + rc.getParam('data');
+		+ '&formCampaignId=' + campaignFormId + '&data=' + rc.getParam('data')
+		+ '#mode=view&form=' + $(action).attr('data-value');
 	window.location = redirectTo;
 };
 
