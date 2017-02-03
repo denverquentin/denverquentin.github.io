fa5bad6ac5b5daeadad5d1ba0e944733884c5a65
Quentin Fish <quentin.fish@roundcorner.com>
2017-02-03 10:55:33 -0700

    asdf

--- rc/js/rc.js
+++ rc/js/rc.js
@@ -2413,7 +2413,7 @@ rc.comp.CreditCard = function(container, data) {
 	// Attach listener to reformat CC
 	this.component.find('[data-name="'+rc.ns+'payment_method_card_number__c"]').on('keyup', rc.comp.CreditCard.format);
 	// Attach listener to add leading 0 to expiration month
-	this.component.find('[data-name="'+rc.ns+'payment_method_card_expiration_month__c"]').on('change', rc.comp.CreditCard.formatExpMonth);
+	this.component.find('[data-name="'+rc.ns+'payment_method_card_expiration_month__c"]').on('onchange', rc.comp.CreditCard.formatExpMonth);
 	//prepopulate values for hidden fields saved along with the form
 	this.component.find('[data-field-hidden="true"]').each(function(index,hiddenField) {
 		var formControlInput = $(hiddenField).find(".form-control");
