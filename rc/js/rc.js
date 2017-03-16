bd905543b1c8a0b7298ad26f0a1baef5e4f26b99
Quentin Fish <quentin.fish@roundcorner.com>
2017-03-16 10:55:18 -0600

    asdf

--- rc/js/rc.js
+++ rc/js/rc.js
@@ -2443,9 +2443,9 @@ rc.comp.CreditCard.format = function() {
 	if (oldData==data) {return;}
 	$(this).val(data);// Save data back to input
 
-	var expMonth = $('[name="'+rc.ns+'payment_method_card_expiration_month__c"]').val();
-	console.log('#expMonth = ' + expMonth);
-	rc.validate.validateField(rc.ns+'payment_method_card_expiration_month__c');//revalidate the field
+//	var expMonth = $('[name="'+rc.ns+'payment_method_card_expiration_month__c"]').val();
+//	console.log('#expMonth = ' + expMonth);
+	rc.validate.validateField(rc.ns+'payment_method_card_number__c');//revalidate the field
 };
 
 rc.comp.CreditCard.formatExpMonth = function() {
@@ -2453,10 +2453,7 @@ rc.comp.CreditCard.formatExpMonth = function() {
 	if (x == '1' || x == '2' || x == '3' || x == '4' || x == '5' || x == '6' || x == '7' || x == '8' || x == '9') {
 		$(this).val('0' + x);// Save data back to input
 	}
-
-	var ccNum = $('[name="'+rc.ns+'payment_method_card_number__c"]').val();
-	console.log('#ccNum = ' + ccNum);
-	rc.validate.validateField(rc.ns+'payment_method_card_number__c');//revalidate the field
+	rc.validate.validateField(rc.ns+'payment_method_card_expiration_month__c');//revalidate the field
 };
 
 rc.comp.Button = function(container, data) {
